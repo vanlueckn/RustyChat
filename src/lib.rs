f3f5eb2de75d69b46aa369389772962490089baa
Tobias Schöler <tobias.schoeler@outlook.de>
2023-06-16 10:20:26 +0200

    chore: update ts sdk to v26

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,48 @@
-#[macro_use]
-extern crate ts3plugin;
-#[macro_use]
-extern crate lazy_static;
-
 use ts3plugin::*;
 
 struct RustyChatTsPlugin;
 
 impl Plugin for RustyChatTsPlugin {
+
+    fn name() -> String {
+        String::from("RustyChat")
+    }
+
+    fn version() -> String {
+        String::from("0.1.0")
+    }
+
+    fn author() -> String {
+        String::from("RustyChat Contributors")
+    }
+
+    fn description() -> String {
+        String::from("Voice Plugin for roleplay servers")
+    }
+
+    fn command() -> Option<String> {
+        Some(String::from("rusty"))
+    }
+
+    fn autoload() -> bool {
+        true
+    }
+
+    fn configurable() -> ConfigureOffer {
+        ConfigureOffer::No
+    }
+
     fn new(api: &mut TsApi) -> Result<Box<Self>, InitError> {
         api.log_or_print("Inited", "RustyChatTsPlugin", LogLevel::Info);
         Ok(Box::new(Self))
     }
-
-    // Implement callbacks here
 
     fn shutdown(&mut self, api: &mut TsApi) {
         api.log_or_print("Shutdown", "MyTsPlugin", LogLevel::Info);
     }
 }
 
-create_plugin!(
-    "RustyChat",
-    "0.1.0",
-    "RustyChat Contributors",
-    "Voice plugin for roleplay servers",
-    ConfigureOffer::No,
-    false,
-    RustyChatTsPlugin
-);
+create_plugin!(RustyChatTsPlugin);
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
