58b56efc69b20f17bf71beaf49870209e234831e
Nils van Lück <nils@vanlueck.dev>
2023-06-16 20:45:25 +0200

    feat: wip settings gui

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,39 +1,90 @@
 use iced::executor;
 use iced::theme::Theme;
-use iced::widget::{button, column, container, text};
+use iced::widget::{button, checkbox, column, container, pick_list, text, text_input};
+use iced::window;
 use iced::Application;
 use iced::{Command, Element, Length};
 
 pub fn show() -> iced::Result {
-    Settings::run(iced::Settings::default())
+    Settings::run(iced::Settings {
+        window: window::Settings {
+            size: (350, 500),
+            ..window::Settings::default()
+        },
+        ..iced::Settings::default()
+    })
 }
 
 pub struct Settings {
     web_socket_address: String,
     instance_timeout: u32,
     is_3d_enabled: bool,
-    phone_offset: StereoMode,
-    radio_offset: StereoMode,
-    secondary_radio_offset: StereoMode,
-    mic_click_mode: MicClickMode,
+    phone_offset: Option<StereoMode>,
+    radio_offset: Option<StereoMode>,
+    secondary_radio_offset: Option<StereoMode>,
+    mic_click_mode: Option<MicClickMode>,
 }
 
-enum StereoMode {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum StereoMode {
     Stereo,
     LeftOnly,
     RightOnly,
 }
 
-enum MicClickMode {
+impl StereoMode {
+    const ALL: [Self; 3] = [Self::Stereo, Self::LeftOnly, Self::RightOnly];
+}
+
+impl std::fmt::Display for StereoMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Stereo => "Stereo",
+                Self::LeftOnly => "Left Only",
+                Self::RightOnly => "Right Only",
+            }
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub enum MicClickMode {
+    #[default]
     ScriptDependent,
     Never,
     Always,
 }
 
-#[derive(Debug, Clone, Copy)]
+impl MicClickMode {
+    const ALL: [Self; 3] = [Self::ScriptDependent, Self::Never, Self::Always];
+}
+
+impl std::fmt::Display for MicClickMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::ScriptDependent => "Script Dependent",
+                Self::Never => "Never",
+                Self::Always => "Always",
+            }
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
 pub enum Message {
-    Save,
-    Close,
+    Ok,
+    WebSocketChanged(String),
+    Has3dChanged(bool),
+    PhoneOffset(StereoMode),
+    RadioOffset(StereoMode),
+    SecondaryRadioOffset(StereoMode),
+    MicClickMode(MicClickMode),
 }
 
 impl Application for Settings {
@@ -49,43 +100,69 @@ impl Application for Settings {
     fn new(_flags: ()) -> (Self, Command<Message>) {
         (
             Self {
-                web_socket_address: String::from(""),
+                web_socket_address: String::from("ws://localhost:31850"),
                 instance_timeout: 0,
                 is_3d_enabled: false,
-                phone_offset: StereoMode::Stereo,
-                radio_offset: StereoMode::Stereo,
-                secondary_radio_offset: StereoMode::Stereo,
-                mic_click_mode: MicClickMode::ScriptDependent,
+                phone_offset: Some(StereoMode::Stereo),
+                radio_offset: Some(StereoMode::Stereo),
+                secondary_radio_offset: Some(StereoMode::Stereo),
+                mic_click_mode: Some(MicClickMode::ScriptDependent),
             },
             Command::none(),
         )
     }
 
     fn view(&self) -> Element<Message> {
-        // We use a column: a simple vertical layout
         let content = column![
-            // The increment button. We tell it to produce an
-            // `IncrementPressed` message when pressed
-            button("+").on_press(Message::Save),
-            // We show the value of the counter here
-            text(self.instance_timeout).size(50),
-            // The decrement button. We tell it to produce a
-            // `DecrementPressed` message when pressed
-            button("-").on_press(Message::Save),
-        ];
+            text("WebSocket URL:"),
+            text_input(&self.web_socket_address, &self.web_socket_address)
+                .on_input(Message::WebSocketChanged),
+            checkbox("3D Audio", self.is_3d_enabled, Message::Has3dChanged),
+            text("Phone Offset:"),
+            pick_list(
+                &StereoMode::ALL[..],
+                self.phone_offset,
+                Message::PhoneOffset
+            ),
+            text("Primary Radio Offset:"),
+            pick_list(
+                &StereoMode::ALL[..],
+                self.radio_offset,
+                Message::RadioOffset
+            ),
+            text("Secondary Radio Offset:"),
+            pick_list(
+                &StereoMode::ALL[..],
+                self.secondary_radio_offset,
+                Message::SecondaryRadioOffset
+            ),
+            text("Mic Click Mode:"),
+            pick_list(
+                &MicClickMode::ALL[..],
+                self.mic_click_mode,
+                Message::MicClickMode
+            ),
+            button("Ok").on_press(Message::Ok),
+        ]
+        .spacing(10);
 
         container(content)
             .width(Length::Fill)
             .height(Length::Fill)
             .center_x()
-            .center_y()
+            .padding(15)
             .into()
     }
 
     fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Message::Save => {}
-            Message::Close => {}
+            Message::Ok => {}
+            Message::WebSocketChanged(value) => self.web_socket_address = value,
+            Message::Has3dChanged(value) => self.is_3d_enabled = value,
+            Message::PhoneOffset(value) => self.phone_offset = Some(value),
+            Message::RadioOffset(value) => self.radio_offset = Some(value),
+            Message::SecondaryRadioOffset(value) => self.secondary_radio_offset = Some(value),
+            Message::MicClickMode(value) => self.mic_click_mode = Some(value),
         }
 
         Command::none()
