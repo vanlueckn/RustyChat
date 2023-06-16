b0b27b73859ed6f955ad529ee7ec2d3a57c95756
Nils van Lück <nils@vanlueck.dev>
2023-06-17 00:25:23 +0200

    feat: add lowpass filter

--- src/audiofx/mod.rs
+++ src/audiofx/mod.rs
@@ -1,5 +1,5 @@
 use aych_delay::{Delay, Settings};
-
+use biquad::*;
 pub fn process_echo(input: &[f32], output: &mut [f32]) {
     let mut delay = Delay::new(Settings {
         delay_time: 166.66,
@@ -13,4 +13,24 @@ pub fn process_echo(input: &[f32], output: &mut [f32]) {
     });
 
     delay.process(input, output);
+}
+
+pub fn process_lowpass(input: &mut [f32]) {
+    // Cutoff and sampling frequencies
+    let f0 = 10.hz();
+    let fs = 1.khz();
+
+    // Create coefficients for the biquads
+    let coeffs =
+        Coefficients::<f32>::from_params(Type::LowPass, fs, f0, Q_BUTTERWORTH_F32).unwrap();
+
+    // Create two different biquads
+    let mut biquad1 = DirectForm1::<f32>::new(coeffs);
+
+    let input_cloned = input.to_vec();
+
+    // Run for all the inputs
+    for (index, t_elem) in input_cloned.iter().enumerate() {
+        input[index] = biquad1.run(*t_elem);
+    }
 }
