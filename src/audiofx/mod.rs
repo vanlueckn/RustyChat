7eb5e52cad86adad56ce8cec5ce8468740344515
Nils van Lück <nils@vanlueck.dev>
2023-06-17 16:43:21 +0200

    fix: fix dsp borrowing

--- src/audiofx/mod.rs
+++ src/audiofx/mod.rs
@@ -1,41 +1,33 @@
-use libdsp_sys::root::DSP::DigitalDelay;
+use libdsp_sys::root::DSP::{BiQuadFilter, DigitalDelay};
 
-struct DelayState {
-    delay: Option<DigitalDelay>,
+const DELAY_AMOUNT: usize = 50;
+const DELAY_FEEDBACK: f32 = 0.7;
+const DELAY_WET: f32 = 0.7;
+const I16_MAX: f32 = (i16::MAX as f32) + 1.0;
+
+fn init_delay() -> DigitalDelay {
+    unsafe { DigitalDelay::new(DELAY_AMOUNT, DELAY_FEEDBACK, DELAY_WET) }
 }
 
-static mut DELAY_STATE: DelayState = DelayState { delay: None };
-
-const DELAY_AMOUNT: usize = 20;
-const DELAY_FEEDBACK: f32 = 0.5;
-const DELAY_WET: f32 = 0.5;
-
-pub fn init_delay() {
+pub fn process_delay(input: &mut [i16], delay: &mut DigitalDelay) {
     unsafe {
-        let mut delay: DigitalDelay = DigitalDelay::new(DELAY_AMOUNT, DELAY_FEEDBACK, DELAY_WET);
-        DELAY_STATE.delay = Some(delay);
-    }
-}
-
-pub fn process_delay(input: &mut [f32]) {
-    unsafe {
-        if DELAY_STATE.delay.is_none() {
-            init_delay();
-        }
-        let mut delay = DELAY_STATE.delay.unwrap();
-
-        for (i, &sample) in input.to_vec().iter().enumerate() {
-            input[i] = delay.getNextSample(sample);
+        for sample in input.iter_mut() {
+            *sample = (delay.getNextSample(*sample as f32 / I16_MAX) * I16_MAX) as i16;
         }
     }
 }
 
-pub fn process_lowpass(input: &mut [f32], alpha: f32) {
-    let mut prev_output = 0.0;
+const CUTOFF_FREQUENCY: f32 = 1000.0;
+const SAMPLE_FREQUENCY: f64 = 48000.0;
 
-    for (i, &sample) in input.to_vec().iter().enumerate() {
-        let filtered_sample = alpha * sample + (1.0 - alpha) * prev_output;
-        input[i] = filtered_sample;
-        prev_output = filtered_sample;
+fn init_lowpass() -> BiQuadFilter {
+    unsafe { BiQuadFilter::new(1, CUTOFF_FREQUENCY as i32, SAMPLE_FREQUENCY) }
+}
+
+pub fn process_lowpass(input: &mut [i16], lowpass: &mut BiQuadFilter) {
+    unsafe {
+        for sample in input.iter_mut() {
+            *sample = (lowpass.nextSample(*sample as f32 / I16_MAX, 1) * I16_MAX) as i16;
+        }
     }
 }
