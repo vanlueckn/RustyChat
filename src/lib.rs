c2d26bd739041003617a5bfa1be526693f0ebd86
Nils van Lück <nils@vanlueck.dev>
2023-06-17 18:56:53 +0200

    fix: fix wrong effect init

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,14 @@ mod gui;
 mod websocket;
 
 use audiofx::init_high_pass;
+use iir_filters::filter::DirectForm2Transposed;
 use ts3plugin::*;
 
-struct RustyChatTsPlugin;
+struct RustyChatTsPlugin {
+    low_pass: DirectForm2Transposed,
+    band_pass: DirectForm2Transposed,
+    high_pass: DirectForm2Transposed,
+}
 
 impl Plugin for RustyChatTsPlugin {
     fn name() -> String {
@@ -43,18 +48,40 @@ impl Plugin for RustyChatTsPlugin {
     fn new(api: &mut TsApi) -> Result<Box<Self>, InitError> {
         api.log_or_print("Inited", "RustyChatTsPlugin", LogLevel::Info);
         websocket::start_listen();
-        Ok(Box::new(Self))
+
+        let low_pass = audiofx::init_lowpass();
+        let band_pass = audiofx::init_band_pass();
+        let high_pass = audiofx::init_high_pass();
+
+        Ok(Box::new(Self {
+            low_pass,
+            band_pass,
+            high_pass,
+        }))
     }
 
-    fn post_process_voice_data(&mut self, api: &mut TsApi, server_id: ServerId,
-            connection_id: ConnectionId, samples: &mut [i16], channels: i32,
-            channel_speaker_array: &[Speaker], channel_fill_mask: &mut u32) {
-                audiofx::process_radio(samples, &mut audiofx::init_band_pass(), &mut init_high_pass());
+    fn post_process_voice_data(
+        &mut self,
+        api: &mut TsApi,
+        server_id: ServerId,
+        connection_id: ConnectionId,
+        samples: &mut [i16],
+        channels: i32,
+        channel_speaker_array: &[Speaker],
+        channel_fill_mask: &mut u32,
+    ) {
+        audiofx::process_radio(samples, &mut self.band_pass, &mut self.high_pass);
     }
 
-    fn captured_voice_data(&mut self, api: &mut TsApi, server_id: ServerId,
-            samples: &mut [i16], channels: i32, send: &mut bool) -> bool {
-        audiofx::process_radio(samples, &mut audiofx::init_band_pass(), &mut init_high_pass());
+    fn captured_voice_data(
+        &mut self,
+        api: &mut TsApi,
+        server_id: ServerId,
+        samples: &mut [i16],
+        channels: i32,
+        send: &mut bool,
+    ) -> bool {
+        audiofx::process_radio(samples, &mut self.band_pass, &mut self.high_pass);
         true
     }
 
